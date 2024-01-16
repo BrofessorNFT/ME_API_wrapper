a7547052ae57787d5633d0db78d12e3cf9bf1587
Brofessor <sovelu11.gmail.com>
2024-01-16 07:31:16 +0100

    UI changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import NavContainer from '@/components/navcontainer';
 import { ThemeProvider } from "@/components/theme-provider"
+import { Analytics } from '@vercel/analytics/react';
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -21,7 +22,9 @@ export default function RootLayout({ children }) {
         >
           <NavContainer/>
           {children}
+          <Analytics />
         </ThemeProvider>
+        
         </body>
     </html>
   )
